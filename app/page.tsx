85a509bed7af6d3de07229d9197dc6987778bc94
Saumya Tiwari <saumyatiwari.29@gmail.com>
2026-04-07 01:11:51 +0530

    feat: remove password gate, load chat app directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { PasswordGate } from '@/components/password-gate'
 import { ChatApp } from '@/components/chat-app'
 
 export default function Page() {
-  return (
-    <PasswordGate>
-      <ChatApp />
-    </PasswordGate>
-  )
+  return <ChatApp />
 }
